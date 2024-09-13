27203fc37d9fd85296446ce088f5145c23f8da55
Schildgard <schildgard@gmail.com>
2024-09-13 09:40:58 +0200

    fixed transition between rooms

--- C++ RPG/C++ RPG/characterController.cpp
+++ C++ RPG/C++ RPG/characterController.cpp
@@ -5,7 +5,7 @@ void MoveCharacter(Character* _player, int*** _level, int _input)
 	int* yDisplacement = &_player->Yposition;
 	int* xDisplacement = &_player->Xposition;
 
-	int targetPositionValue = CheckTargetPosition(_player, _level, _input, yDisplacement,xDisplacement);
+	int targetPositionValue = CheckTargetPosition(_player, _level, _input, yDisplacement, xDisplacement);
 
 	switch (targetPositionValue)
 	{
@@ -19,15 +19,18 @@ void MoveCharacter(Character* _player, int*** _level, int _input)
 		_player->Xposition = *xDisplacement;
 		break;
 	case 3:
-		GameManager::roomCount = GameManager::currentRoom + 2; //+2 because currentRoomIndex starts at 0 while roomCount starts at 1
+		if (GameManager::roomCount < 3)
+		{
+			GameManager::roomCount = GameManager::currentRoom + 2; //+2 because currentRoomIndex starts at 0 while roomCount starts at 1
+		}
 		GameManager::currentRoom++;
 		if (GameManager::currentRoom == 1)_player->Xposition = 1; // funktioniert in level 1, in level 2 aber nicht mehr
-		//else if (GameManager::currentRoom == 2) _player->Yposition = 1;
+		else if (GameManager::currentRoom == 2) _player->Yposition = 1;
 		break;
 	case 4:
 		GameManager::currentRoom--;
-		if (GameManager::currentRoom == 1)_player->Xposition = GameManager::levelWidth - 2; // funktioniert von level 2 in level 1, aber von level 3 in 2 wird das nicht funktionieren
-		else if (GameManager::currentRoom == 2) _player->Yposition = GameManager::levelHeight -2;
+		if (GameManager::currentRoom == 0)_player->Xposition = GameManager::levelWidth - 2; // funktioniert von level 2 in level 1, aber von level 3 in 2 wird das nicht funktionieren
+		else if (GameManager::currentRoom == 1) _player->Yposition = GameManager::levelHeight - 2;
 		break;
 
 
@@ -40,7 +43,7 @@ void MoveCharacter(Character* _player, int*** _level, int _input)
 int CheckTargetPosition(Character* _player, int*** _level, int _input, int* _yDisplacement, int* _xDisplacement)
 {
 	int currentPositionValue = _level[GameManager::currentRoom][_player->Yposition][_player->Xposition];
-	int targetPositionValue =1;
+	int targetPositionValue = 1;
 
 
 	switch (_input)
@@ -53,7 +56,7 @@ int CheckTargetPosition(Character* _player, int*** _level, int _input, int* _yDi
 		}
 		break;
 	case 80: //DOWN
-		if (_player->Yposition < GameManager::levelHeight - 2 || _level[GameManager::currentRoom][_player->Yposition+1][_player->Xposition ] == 3) //check if player is on lower edge
+		if (_player->Yposition < GameManager::levelHeight - 2 || _level[GameManager::currentRoom][_player->Yposition + 1][_player->Xposition] == 3) //check if player is on lower edge
 		{
 			targetPositionValue = _level[GameManager::currentRoom][_player->Yposition + 1][_player->Xposition];
 			*_yDisplacement = _player->Yposition++;
